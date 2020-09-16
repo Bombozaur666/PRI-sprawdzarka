f7c8122c70ed4a97fa4711681e4cc6a6be1ad5be
bombozaur <43881316+bombozaur@users.noreply.github.com>
2020-09-16 16:42:48 +0200

    Update views.py
    ading views

--- sprawdzarka/upload/views.py
+++ sprawdzarka/upload/views.py
@@ -3,7 +3,9 @@ from rest_framework import permissions
 from django.shortcuts import render, redirect
 from upload import models
 from .forms import SendedTasksForm
+from .forms import TasksListForm
 from .models import SendedTasks
+from .models import TaskList
 from api import serializers
 from django.contrib.auth.models import User, Group
 from django.http import HttpResponse
@@ -29,9 +31,27 @@ def task_sended_upload(request):
         form=SendedTasksForm()
     return render(request,'task_sended_upload.html', {'form': form})
 
-def read_file(request, file_to_open):
+def read_file1(request, file_to_open):
     f = open(r'task/SendedTasks/'+file_to_open, 'r')
     file_content = f.read()
     f.close()
     return HttpResponse(file_content, content_type="text/plain")
 
+def task_list(request):
+    sended=TaskList.objects.all
+    return render(request,'task_List.html',{'sended': sended})
+
+def task_List_upload(request):
+    if request.method=='POST':
+        form = TasksListForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form=TasksListForm()
+    return render(request,'task_sended_upload.html', {'form': form})
+
+def read_file2(request, file_to_open):
+    f = open(r'task/TaskList/'+file_to_open, 'r')
+    file_content = f.read()
+    f.close()
+    return HttpResponse(file_content, content_type="text/plain")
